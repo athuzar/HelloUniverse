071d9d14e74960a9f49534ae2c0257714d9b2ac7
athuzar <athuzar@pingry.org>
2018-09-06 01:23:12 -0400

    My first commit

---
+++ HelloUniverse.java
@@ -0,0 +1,13 @@
+// Aye Thuzar
+// 09/06/2018
+// prints out the String "Hello Universe" on screen
+
+public class HelloUniverse
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Universe");
+	}
+
+
+}
